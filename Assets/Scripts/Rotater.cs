bf36f10fe7c9fae28145f986a9053c2da7511d55
shubuzhi <yhe1993@gmail.com>
2018-07-05 01:23:10 -0400

    update

---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotater : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (10, 30, 40) * Time.deltaTime);
+	}
+}
